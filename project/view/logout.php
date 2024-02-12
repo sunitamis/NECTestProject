6c3005383a3a6bc8a9cba192c0077226f844f96b
sunitamis <sunitamistry8@gmail.com>
2024-02-12 07:26:01 +0530

    Deleting old one and adding a new updated one

--- project/view/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-	session_start();//session starts here
-	unset($_SESSION['uname']);//all session variables gets destroyed here
-	session_destroy();//session gets destroyed here
-	header("location:login.php");//heads to login page
-?>
